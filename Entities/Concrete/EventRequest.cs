07e5ae1b57e28a1c0408e207301cf85ac89a41bd
zenci32 <bkayran32@gmail.com>
2023-03-30 23:34:31 +0300

    duzenleme

---
+++ Entities/Concrete/EventRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class EventRequest
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public int InviterPhone { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
